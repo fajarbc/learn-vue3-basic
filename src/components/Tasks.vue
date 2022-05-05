63faf4afd126aa855ddb7dd80052a740815816bc
Fajar BC <bcfajar@gmail.com>
2022-05-05 08:36:49 +0700

    Minor wrapp v-for inside div

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,19 +1,25 @@
 <template>
+  <div>
     <div :key="task.id" v-for="task in tasks">
-        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
+      <Task
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-import Task from './Task.vue'
+import Task from "./Task.vue";
 export default {
-    name: "Tasks",
-    props: {
-        tasks: Array,
-    },
-    components: {
-        Task,
-    },
-    emits: ['delete-task', 'toggle-reminder'],
-}
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
 </script>
